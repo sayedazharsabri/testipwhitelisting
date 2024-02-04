f9dc36cb0537eb29d287576a18528b7644f6652b
Sayed Azhar Sabri <afzal.sayed26@gmail.com>
2024-02-04 17:06:00 +0100

    inti

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+// Import necessary modules
+import express, { Request, Response } from "express";
+import axios from "axios";
+
+// Create an Express application
+const app = express();
+const port = 3000;
+
+// Define a route to make a GET request to 'https://ipdomainipwhitelisting.onrender.com/'
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    // Make a GET request using axios
+    const response = await axios.get(
+      "https://ipdomainipwhitelisting.onrender.com/"
+    );
+
+    // Send the result back to the client
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    // Handle errors
+    res.status(500).send("Error fetching data");
+  }
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
